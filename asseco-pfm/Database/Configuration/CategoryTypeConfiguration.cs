68f230ff8cc04ffd7f5f91695dc85c9fd43e15ee
AzureSavant <Aleksandar.Nonevski@students.finki.ukim.mk>
2022-07-20 23:49:49 +0200

    Create Transaction and Category DB configurations

---
+++ asseco-pfm/Database/Configuration/CategoryTypeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using asseco_pfm.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace asseco_pfm.Database.Configuration
+{
+    public class CategoryTypeConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("category");
+            builder.HasKey(x => x.Code);
+            builder.Property(x => x.Code).IsRequired(true);
+            builder.Property(x => x.ParentCode).HasMaxLength(2);
+            builder.Property(x => x.Name).IsRequired(true);
+        }
+    }
+}
---
+++ asseco-pfm/Database/Configuration/TransactionTypeConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using asseco_pfm.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace asseco_pfm.Database.Configuration
+{
+    public class TransactionTypeConfiguration : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.ToTable("transaction");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).IsRequired(true);
+            builder.Property(x => x.BeneficaryName).HasMaxLength(40);
+            builder.Property(x => x.Date).IsRequired(true);
+            builder.Property(x => x.Direction).HasConversion<string>().IsRequired(true);
+            builder.Property(x => x.Ammount).IsRequired(true);
+            builder.Property(x => x.Description).HasMaxLength(100);
+            builder.Property(x => x.Currency).HasMaxLength(3).IsRequired(true);
+            builder.Property(x => x.Mcc).IsRequired(false);
+            builder.Property(x => x.Kind).HasConversion<string>().IsRequired(true);
+            builder.Property(x => x.Catcode);
+        }
+    }
+}
