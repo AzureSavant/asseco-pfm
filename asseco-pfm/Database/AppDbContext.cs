ae49af9b74551dc0bc87fbe272b2b6b7c3581e1b
AzureSavant <Aleksandar.Nonevski@students.finki.ukim.mk>
2022-07-21 16:22:21 +0200

    Add App DB Context

---
+++ asseco-pfm/Database/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using asseco_pfm.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace asseco_pfm.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Transaction> Transaction { get; set; }
+        public DbSet<Category> Category { get; set; }
+
+        public AppDbContext()
+        {
+
+        }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
