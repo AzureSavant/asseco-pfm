edabeee8a632051ea16542e0680707f9a65aa10b
AzureSavant <Aleksandar.Nonevski@students.finki.ukim.mk>
2022-07-26 00:53:10 +0200

    Refactor DTOs, Add TransactionDTO

---
+++ asseco-pfm/DTO/TransactionDto.cs
@@ -0,0 +1,18 @@
+﻿using asseco_pfm.Models;
+
+namespace asseco_pfm.DTO
+{
+    public class TransactionDto
+    {
+        public string? BeneficaryName { get; set; }
+        public DateTime Date { get; set; }
+        public TransactionDirectionEnum Direction { get; set; }
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+        public string Currency { get; set; }
+        public int? Mcc { get; set; }
+        public TransactionKindEnum Kind { get; set; }
+        public string? Catcode { get; set; }
+        public List<TransactionSplitSingle>? Splits { get; set; }
+    }
+}
