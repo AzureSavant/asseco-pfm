82714877f994f208f3a5cee6a53142cf44afb792
AzureSavant <Aleksandar.Nonevski@students.finki.ukim.mk>
2022-07-30 00:23:09 +0200

    Add Spendings for Category DTOs

---
+++ asseco-pfm/DTO/SpendingInCategory.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+
+namespace asseco_pfm.DTO
+{
+    public class SpendingInCategory
+    {
+        [DataMember(Name = "catcode")]
+        public string Catcode { get; set; }
+        [DataMember(Name = "amount")]
+        public decimal? Amount { get; set; }
+        [DataMember(Name = "count")]
+        public int? Count { get; set; }
+
+        public SpendingInCategory(string? catcode, decimal amount, int count)
+        {
+            Catcode = catcode;
+            Amount = amount;
+            Count = count;
+        }
+    }
+}
---
+++ asseco-pfm/DTO/SpendingsByCategoryDto.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace asseco_pfm.DTO
+{
+    public class SpendingsByCategoryDto
+    {
+        public List<SpendingInCategory> Groups { get; set; }
+
+        public SpendingsByCategoryDto()
+        {
+            Groups = new List<SpendingInCategory>();
+        }
+    }
+}
